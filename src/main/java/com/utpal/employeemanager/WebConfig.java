ce6d5c1b2f18d0be69d68de7a18dd971b813486d
utpal-kumar-singh <utpalsingh2002@gmail.com>
2025-08-05 16:42:04 +0530

    Updated base URL to render backend

--- src/main/java/com/utpal/employeemanager/WebConfig.java
+++ src/main/java/com/utpal/employeemanager/WebConfig.java
@@ -6,17 +6,12 @@ import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
-public class WebConfig {
-
-    @Bean
-    public WebMvcConfigurer corsConfigurer() {
-        return new WebMvcConfigurer() {
-            @Override
-            public void addCorsMappings(CorsRegistry registry) {
-                registry.addMapping("/employee/**")
-                        .allowedOrigins("http://localhost:3000")
-                        .allowedMethods("GET", "POST", "PUT", "DELETE");
-            }
-        };
+public class WebConfig implements WebMvcConfigurer {
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("https://utpal-kumar-singh.github.io")
+                .allowedMethods("*");
     }
 }
+
